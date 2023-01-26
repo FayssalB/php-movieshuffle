17d44d2b1787582b67916608867e0e0f1e3cc894
Fayssal Belghazi <faysstoface@hotmail.fr>
2023-01-26 17:31:31 +0100

    Pagination en cours

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,25 @@
+<div class="pagination-container">
+    <?php
+    if ($nombre_pages > $nombre_element_par_page) {
+        for ($i = 1; $i <= $nombre_pages; $i++) {
+    ?>
+        
+        <a class="pagination-link" href=""><?= $i ?></a>
+        
+    <?php
+        }
+    } ?>
+
 </div>
+
 </body>
+
+<script>
+    balisei = document.getElementById("i");
+    inpt = document.getElementById("in");
+    console.log(inpt);
+    console.log(balisei);
+    balisei.onclick = () => inpt.classList.toggle("active");
+</script>
 
 </html>
--- templates/header.php
+++ templates/header.php
@@ -2,11 +2,6 @@
 $dsn = "mysql:host=localhost;dbname=movieshuffle";
 $db = new PDO($dsn, "root", "");
 
-
-
-// $recherche = "";
-// $recher = trim(strip_tags($_GET[$recherche]));
-
 $data = array("name" => "Recette introuvable");
 
 @$keywords = $_GET["keywords"];
@@ -15,7 +10,14 @@ $data = array("name" => "Recette introuvable");
 if (!empty($keywords)) {
     $query = $db->prepare("SELECT * FROM movieslist where title like '%$keywords%'");
     $query->execute();
-    $tab = $query->fetchAll();
+    $tab = $query->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($tab);
+    
+    $nombre_element_par_page=3;
+    $nombre_pages = ceil(count($tab)/$nombre_element_par_page);
+    echo $nombre_pages;
+
+
 }
 
 
@@ -28,6 +30,7 @@ if (!empty($keywords)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/main.css">
     <title>Movie3000</title>
 </head>
@@ -37,18 +40,15 @@ if (!empty($keywords)) {
         <div>
             <h2><a href="index.php">MovieShuffle</a></h2>
         </div>
-        <div >
-            <form class="input-container" name="fo" action="" method="GET">
-                <p id="pi" onclick="">🔍</p>
-               
-                <input id="in" type="text" name="keywords" placeholder="ICI" >
+        <div>
+            <form class="input-container" action="" method="GET">
+                
+                <i id="i" class="fa-solid fa-magnifying-glass"></i>
+                <input id="in" type="text" name="keywords" >
 
             </form>
-           
+
         </div>
     </div>
 
-    <script>
-        p = document.getElementById("pi");
-        p.onclick = ()=> document.getElementById("in").classList.toggle("active");
-    </script>
+    
